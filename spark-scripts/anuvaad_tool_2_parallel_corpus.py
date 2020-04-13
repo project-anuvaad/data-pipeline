0f697f0a854880bf6f2fb7d78b23e6f0fa7a9ab9
Senthil Kumar <senthil.muthuvinayagam@tarento.com>
2020-04-13 18:56:31 +0530

    modified tool2 to use hbase integration util

--- spark-scripts/anuvaad_tool_2_parallel_corpus.py
+++ spark-scripts/anuvaad_tool_2_parallel_corpus.py
@@ -7,16 +7,18 @@
 ######################################################
 
 
-import nltk
+
 import pyspark.sql.functions as F
 from pyspark import AccumulatorParam
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, ArrayType, \
     StringType
 from googletrans import Translator
+import tool2_hbase_util as hbase
 import csv
 import requests
 import json
+from pyspark import SparkConf, SparkContext
 
 
 
@@ -26,26 +28,33 @@ def get_model_translation(word):
     body=[{'src': word, 'id': 56 }]
     response = requests.post(url,json=body)
     data = response.json()
-    target = data['response_body'][0]['tgt']
-    return target
+    tgt = data['response_body'][0]['tgt']
+    return tgt
 
 
 translator = Translator()
-spark = SparkSession.builder.appName('Anuvaad_Sentence_Translator'
-        ).config('spark.executor.memory', '2g').getOrCreate()
+conf = SparkConf().setAppName("spark-test").setMaster("local")
+sc = SparkContext(conf=conf)
+spark = SparkSession.builder.config('spark.executor.memory', '1g').getOrCreate()
 
 LOG4JLOGGER = spark.sparkContext._jvm.org.apache.log4j
 LOGGER = LOG4JLOGGER.LogManager.getLogger(__name__)
 LOGGER.info('=======================================================')
 LOGGER.info('Starting the Sentence Translation process for Anuvaad...')
 LOGGER.info('=======================================================')
-
-
+sc.addFile("tool2_hbase_util.py")
 
 # READ INPUT FILE
 def parseLine(line):
+    
     source = line
-    target = get_model_translation(source[0])
+    target = hbase.get_translation(source[0],"en","hi")
+    if target:
+        print('Translated text from HBASE')
+    else:
+        print('Translation not found')
+        target = get_model_translation(source[0])
+        hbase.store_translation(source[0],target,"en","hi")
     return (source[0], target)
 
 
@@ -55,12 +64,16 @@ DF_INPUT_FILE = spark.read.format('com.databricks.spark.csv'
                                   ).option('escape', '"'
         ).option('header', 'false').option('delimiter', ','
         ).option('quote', '"').option('encoding', 'cp1252'
-        ).schema(SCHEMA_INPUT_FILE).load('file:///SparkCourse/escape_test.csv')
+        ).schema(SCHEMA_INPUT_FILE).load('*.csv')
 parsedLines = DF_INPUT_FILE.rdd.map(parseLine)
 df = spark.createDataFrame(parsedLines)
 
 #df.show()
-df.coalesce(1).write.option("encoding", "utf-8").mode("overwrite").format("com.databricks.spark.csv").save("file:///SparkCourse/translatedcsv.csv")
 
+df.coalesce(1).write.option("encoding", "utf-8").mode("overwrite").format("com.databricks.spark.csv").save("/user/root/translated/en_hi_v2.csv")
 
 LOGGER.info('Successfully translated the Sentences.')
+
+
+
+
