1900e43db30924896e791b94f8b1e8041c33bf7a
Senthil Kumar <senthil.muthuvinayagam@tarento.com>
2020-04-20 13:02:04 +0530

    Added Multiple File Support and write file based on the file names and code formatting with configuration file

--- spark-scripts/anuvaad_tool_2_parallel_corpus.py
+++ spark-scripts/anuvaad_tool_2_parallel_corpus.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 ######################################################
 # PROJECT : Anuvaad Sentence Translator
@@ -9,22 +8,58 @@
 
 
 import pyspark.sql.functions as F
-from pyspark import AccumulatorParam
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, ArrayType, \
-    StringType
+from pyspark.sql.types import StructType, StructField, ArrayType,StringType
 from googletrans import Translator
-import tool2_hbase_util as hbase
+import hbase_spark as hbase
 import csv
 import requests
 import json
+import sys
 from pyspark import SparkConf, SparkContext
+from pyspark.sql.functions import regexp_extract, input_file_name
+from ruamel import yaml
 
+'''
+------------------------------------------------------
+Intialalize Spark Context Google Translator and LOGGER
+------------------------------------------------------
+'''
+translator = Translator()
+conf = SparkConf().setAppName("spark-test").setMaster("local")
+sc = SparkContext(conf=conf)
+spark = SparkSession.builder.config('spark.executor.memory', '1g').getOrCreate()
+LOG4JLOGGER = spark.sparkContext._jvm.org.apache.log4j
+LOGGER = LOG4JLOGGER.LogManager.getLogger(__name__)
+LOGGER.info(sc.getConf().getAll())
 
+'''
+---------------------------------------
+LOAD CONFIG FILE
+---------------------------------------
+'''
+LOGGER.info("Following config passed to the Driver :")
 
+ANUVAAD_INPUT_CONFIG= yaml.safe_load(open('config.yml'))
+
+'''
+---------------------------------------
+Get config values from yml file
+---------------------------------------
+'''
+url = ANUVAAD_INPUT_CONFIG["url"]
+source_language = ANUVAAD_INPUT_CONFIG["source_language"]
+target_language = ANUVAAD_INPUT_CONFIG["target_language"]
+input_file_folder = ANUVAAD_INPUT_CONFIG["source_folder"]
+regex_pattern = ANUVAAD_INPUT_CONFIG["regex_pattern"]
+output_file_folder = ANUVAAD_INPUT_CONFIG["target_folder"]
+
+'''
+-------------------------------------------------------
+Translator function using Tarento model translation API
+-------------------------------------------------------
+'''
 def get_model_translation(word):
-    
-    url = "http://52.40.71.62:3003/translator/translate-anuvaad"
     body=[{'src': word, 'id': 56 }]
     response = requests.post(url,json=body)
     data = response.json()
@@ -32,45 +67,67 @@ def get_model_translation(word):
     return tgt
 
 
-translator = Translator()
-conf = SparkConf().setAppName("spark-test").setMaster("local")
-sc = SparkContext(conf=conf)
-spark = SparkSession.builder.config('spark.executor.memory', '1g').getOrCreate()
 
-LOG4JLOGGER = spark.sparkContext._jvm.org.apache.log4j
-LOGGER = LOG4JLOGGER.LogManager.getLogger(__name__)
 LOGGER.info('=======================================================')
 LOGGER.info('Starting the Sentence Translation process for Anuvaad...')
 LOGGER.info('=======================================================')
-sc.addFile("tool2_hbase_util.py")
 
-# READ INPUT FILE
+'''
+------------------------------------------------------
+Import the hbase-integration module
+------------------------------------------------------
+'''
+sc.addFile("hbase_spark.py")
+
+'''
+------------------------------------------------------
+Parse all the lines and translate the sentences
+------------------------------------------------------
+'''
 def parseLine(line):
     
-    source = line
-    target = hbase.get_translation(source[0],"en","hi")
+    target = hbase.get_translation(line[0],source_language,target_language)
     if target:
         print('Translated text from HBASE')
     else:
         print('Translation not found')
-        target = get_model_translation(source[0])
-        hbase.store_translation(source[0],target,"en","hi")
-    return (source[0], target)
+        target = get_model_translation(line[0])
+        hbase.store_translation(line[0],target,source_language,target_language)
+    return (line[0], target,line[1])
 
+'''
+------------------------------------------------------
+Input File Structure
+------------------------------------------------------
+'''
 
 SCHEMA_INPUT_FILE = StructType([StructField('sentences', StringType(),
                                True)])
+
+'''
+------------------------------------------------------
+Parse all the input files and convert the data to data frame
+------------------------------------------------------
+'''
+
 DF_INPUT_FILE = spark.read.format('com.databricks.spark.csv'
                                   ).option('escape', '"'
         ).option('header', 'false').option('delimiter', ','
         ).option('quote', '"').option('encoding', 'cp1252'
-        ).schema(SCHEMA_INPUT_FILE).load('*.csv')
-parsedLines = DF_INPUT_FILE.rdd.map(parseLine)
-df = spark.createDataFrame(parsedLines)
+        ).schema(SCHEMA_INPUT_FILE).load(input_file_folder)
 
-#df.show()
 
-df.coalesce(1).write.option("encoding", "utf-8").mode("overwrite").format("com.databricks.spark.csv").save("/user/root/translated/en_hi_v2.csv")
+df2 = DF_INPUT_FILE.withColumn("file_name", F.split(regexp_extract(input_file_name(),regex_pattern,1),'/')[1])
+df2.show()
+parsedLines = df2.rdd.map(parseLine)
+df = spark.createDataFrame(parsedLines).toDF("source_sentence", "target_sentence","file_name")
+
+'''
+---------------------------------------------------------------------
+Generate the parallel corpus files partitioned by the input file name
+---------------------------------------------------------------------
+'''
+df.coalesce(1).write.option("encoding", "utf-8").mode("overwrite").format("com.databricks.spark.csv").partitionBy("file_name").save(output_file_folder)
 
 LOGGER.info('Successfully translated the Sentences.')
 
--- spark-scripts/tool2_hbase_util.py
+++ spark-scripts/tool2_hbase_util.py
@@ -1,52 +1,84 @@
 import happybase
 import hashlib
-connection = happybase.Connection('sandbox-hdp.hortonworks.com')
+from pyspark.sql import SparkSession
+
+'''
+------------------------------------------------------
+Intialalize Spark and LOGGER
+------------------------------------------------------
+'''
+#spark = SparkSession.builder.config('spark.executor.memory', '1g').getOrCreate()
+
+#LOG4JLOGGER = spark.sparkContext._jvm.org.apache.log4j
+#LOGGER = LOG4JLOGGER.LogManager.getLogger(__name__)
+
+'''
+---------------------------------------
+Get config values from yml file
+---------------------------------------
+'''
+#hbase_server = ANUVAAD_INPUT_CONFIG[hbase_server]
+#hbase_table = ANUVAAD_INPUT_CONFIG[hbase_table]
+hbase_server = "sandbox-hdp.hortonworks.com"
+hbase_table = "translated_sentences"
+
+
+'''
+---------------------------------------
+Connection to Hbase server
+---------------------------------------
+'''
+connection = happybase.Connection(hbase_server)
 connection.open()
-table = connection.table('translated_sentences')
+table = connection.table(hbase_table)
 
-
+'''
+---------------------------------------
+Function for Storing the translation
+---------------------------------------
+'''
 def store_translation(source_sentence, target_sentence, source_language, target_language):
     
     print('store_translation for: '+ source_language + '-' + target_language )
     try:
-        source_language="en"
+        
         source_sentence = source_sentence
-        target_sentence = target_sentence
-        target_language = "hi"
-
+        target_sentence = target_sentence.encode('UTF-8','strict')
         encoded_str = hashlib.sha256(source_sentence.encode())
         hash_hex = encoded_str.hexdigest()
-        print(hash_hex)
         row_key=target_language+"_"+hash_hex
         print(row_key) 
         table.put(row_key, {"info:source_sentence": source_sentence})
         table.put(row_key, {"info:target_sentence": target_sentence})
-        table.put(row_key, {"info:source_language": "en"})
+        table.put(row_key, {"info:source_language": source_language})
         table.put(row_key, {"info:target_language": target_language})        
 
     except Exception as e:
         print('store_translation : Error occurred in storing the translation '', error is == ' + str(e))
-
+'''
+-----------------------------------------------------
+Function for getting the translation from HBASE table
+-----------------------------------------------------
+'''
 
 def get_translation(source_sentence, source_language, target_language):
 
     print('get_translation for: '+source_language + '-' + target_language )
     try:
-        source_language="en"
         source_sentence = source_sentence
-        target_language = "hi"
-
         encoded_str = hashlib.sha256(source_sentence.encode())
         hash_hex = encoded_str.hexdigest()
         print(hash_hex)
+
+
         row_key=target_language+"_"+hash_hex
+        
         print(row_key) 
-        row = table.row(row_key)      
-        translated_sentence = row[b'info:target_sentence']
+        row = table.row(row_key)
+        if row:      
+           translated_sentence = row[b'info:target_sentence'].decode('UTF-8','strict')
+        else:
+           translated_sentence=""
         return translated_sentence
     except Exception as e:
-        print('store_translation : Error occurred in getting the translation '', error is == ' + str(e))
-
-
-
-
+        print('get_translation : Error occurred in getting the translation '', error is == ' + str(e))
