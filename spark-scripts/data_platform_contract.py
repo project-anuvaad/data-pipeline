374d9ebabf2950d9924a862fdff6fc57dd8b763f
Aravinth Bheemaraj <b.aravinth@gmail.com>
2020-07-10 14:41:50 +0530

    Code cleanup.

--- spark-scripts/data_platform_contract.py
+++ spark-scripts/data_platform_contract.py
@@ -8,7 +8,6 @@
 
 
 import pyspark.sql.functions as F
-
 from pyspark.sql.types import StructType, StructField, StringType
 
 SCHEMA_INPUT_FILE = StructType([ \
@@ -33,22 +32,21 @@ metadata_schema =  StructType([ StructField("src_filename", StringType(), True),
 
 df = df.withColumn("metadata_json_parsed", F.from_json(df["metadata_json"], metadata_schema))
 
-df = df.select("sno", "src", "tgt", "src_lang", "tgt_lang", \
-	          "metadata_json_parsed.src_filename", \
-	          "metadata_json_parsed.tgt_filename", \
-	          "metadata_json_parsed.label")
+df = df.select(F.sha2(F.col("src"), 256).alias("src_hash"), "src", "tgt", "src_lang", "tgt_lang", \
+              "metadata_json_parsed.src_filename", \
+              "metadata_json_parsed.tgt_filename", \
+              "metadata_json_parsed.label")
 
-df.write.parquet("/tmp/proto.parquet")
+df.write.parquet("/user/root/hive/proto/proto.parquet")
 
 
 ######################################################
 # Exposing in spark-sql
 ######################################################
-#  
 #  > create database test_db;
 #  > use test_db;
 #  > create table sentences_parquet(
-#      sno string,
+#      src_hash string,
 #      src string,
 #      tgt string,
 #      src_lang string,
@@ -57,13 +55,13 @@ df.write.parquet("/tmp/proto.parquet")
 #      tgt_filename string,
 #      label string)
 #    stored as parquet 
-#    location '/tmp/proto.parquet';
+#    location '/user/root/hive/proto/proto.parquet';
 #  
 #  > desc sentences_parquet;
 #  +---------------+------------+----------+
 #  |   col_name    | data_type  | comment  |
 #  +---------------+------------+----------+
-#  | sno           | string     |          |
+#  | src_hash      | string     |          |
 #  | src           | string     |          |
 #  | tgt           | string     |          |
 #  | src_lang      | string     |          |
@@ -97,34 +95,54 @@ df.write.parquet("/tmp/proto.parquet")
 #  spark.sql("select src, tgt from sentences_parquet2 where label='M' limit 10").show()
 #  
 #  
-#  >>> df = spark.sql("select src, tgt, label from sentences_parquet2 where length(src) < 40")
-#  >>> df.filter("label='M'").select("src", "tgt").show(10, False)
-#  +--------------------------------------+-------------------------------------+
-#  |src                                   |tgt                              	|
-#  +--------------------------------------+-------------------------------------+
-#  |B. B.                                 |बी.                             		|
-#  |Tiwari passed away on 25 April, 2012. |तिवारी का 25 अप्रैल, 2012 को निधन हो गया। 	|
-#  |Working of 3 Ministries was discussed.|तीन मंत्रालयों के कार्यकरण पर चर्चा हुई।      	|
-#  |Some time was lost in disruptions;    |थोड़ा समय व्यवधान में नष्ट हुआ;            	|
-#  |Even they were not discussed.         |इन पर भी चर्चा नहीं हुई।                  	|
-#  |Private Members introduced 33 Bills.  |गैर-सरकारी सदस्यों ने 33 विधेयक पुरःस्थापित किए 	|
-#  |This has been a difficult year.       |यह एक कठिन वर्ष रहा है।              		|
-#  |In all, the House sat for 22 sittings.|कुल मिलाकर सभा की 22 बैठकें हुईं हैं।       	|
-#  |This has been a difficult year.       |यह एक कठिन वर्ष रहा है।            		|
-#  |That must really inspire us.          |हमें वास्तव में उससे प्रेरित होना चाहिए।     		|
-#  +--------------------------------------+-------------------------------------+
+#   >>> df = spark.sql("select src, tgt, label from sentences_parquet where length(src) < 40")
+#   >>> df.filter("label='M'").select("src", "tgt").show(10, False)
+#   +--------------------------------------+-------------------------------------+
+#   |src                                   |tgt                              	|
+#   +--------------------------------------+-------------------------------------+
+#   |B. B.                                 |बी.                             		|
+#   |Tiwari passed away on 25 April, 2012. |तिवारी का 25 अप्रैल, 2012 को निधन हो गया। 	|
+#   |Working of 3 Ministries was discussed.|तीन मंत्रालयों के कार्यकरण पर चर्चा हुई।      	|
+#   |Some time was lost in disruptions;    |थोड़ा समय व्यवधान में नष्ट हुआ;            	|
+#   |Even they were not discussed.         |इन पर भी चर्चा नहीं हुई।                  	|
+#   |Private Members introduced 33 Bills.  |गैर-सरकारी सदस्यों ने 33 विधेयक पुरःस्थापित किए 	|
+#   |This has been a difficult year.       |यह एक कठिन वर्ष रहा है।              		|
+#   |In all, the House sat for 22 sittings.|कुल मिलाकर सभा की 22 बैठकें हुईं हैं।       	|
+#   |This has been a difficult year.       |यह एक कठिन वर्ष रहा है।            		|
+#   |That must really inspire us.          |हमें वास्तव में उससे प्रेरित होना चाहिए।     		|
+#   +--------------------------------------+-------------------------------------+
+#   
+#   
+#   spark-sql> select src, tgt, label from sentences_parquet where length(src) < 40 and label='M' limit 10;
+#   
+#   B. B.	बी.	M
+#   Tiwari passed away on 25 April, 2012.	तिवारी का 25 अप्रैल, 2012 को निधन हो गया।	M
+#   Working of 3 Ministries was discussed.	तीन मंत्रालयों के कार्यकरण पर चर्चा हुई।	M
+#   Some time was lost in disruptions;	थोड़ा समय व्यवधान में नष्ट हुआ;	M
+#   Even they were not discussed.	इन पर भी चर्चा नहीं हुई।	M
+#   Private Members introduced 33 Bills.	गैर-सरकारी सदस्यों ने 33 विधेयक पुरःस्थापित किए।	M
+#   This has been a difficult year.	यह एक कठिन वर्ष रहा है।	M
+#   In all, the House sat for 22 sittings.	कुल मिलाकर सभा की 22 बैठकें हुईं हैं।	M
+#   This has been a difficult year.	यह एक कठिन वर्ष रहा है।	M
+#   That must really inspire us.	हमें वास्तव में उससे प्रेरित होना चाहिए।	M
+#
+######################################################
+# Loading into HBase
+######################################################
+#  CREATE EXTERNAL TABLE hbase_table_sentences
+#    (
+#       src_hash string,
+#       src string,
+#       tgt string,
+#       src_lang string,
+#       tgt_lang string,
+#       src_filename string,
+#       tgt_filename string,
+#       label string
+#    )
+#  STORED BY 'org.apache.hadoop.hive.hbase.HBaseStorageHandler'
+#  WITH SERDEPROPERTIES ("hbase.columns.mapping" = ":key,cf:src,cf:tgt,cf:src_lang,cf:tgt_lang,cf:src_filename,cf:tgt_filename,cf:label")
+#  TBLPROPERTIES ("hbase.table.name" = "hbase_sentences");
 #  
 #  
-#  spark-sql> select src, tgt, label from sentences_parquet2 where length(src) < 40 and label='M' limit 10;
-#  
-#  B. B.	बी.	M
-#  Tiwari passed away on 25 April, 2012.	तिवारी का 25 अप्रैल, 2012 को निधन हो गया।	M
-#  Working of 3 Ministries was discussed.	तीन मंत्रालयों के कार्यकरण पर चर्चा हुई।	M
-#  Some time was lost in disruptions;	थोड़ा समय व्यवधान में नष्ट हुआ;	M
-#  Even they were not discussed.	इन पर भी चर्चा नहीं हुई।	M
-#  Private Members introduced 33 Bills.	गैर-सरकारी सदस्यों ने 33 विधेयक पुरःस्थापित किए।	M
-#  This has been a difficult year.	यह एक कठिन वर्ष रहा है।	M
-#  In all, the House sat for 22 sittings.	कुल मिलाकर सभा की 22 बैठकें हुईं हैं।	M
-#  This has been a difficult year.	यह एक कठिन वर्ष रहा है।	M
-#  That must really inspire us.	हमें वास्तव में उससे प्रेरित होना चाहिए।	M
-
+#  INSERT INTO TABLE hbase_table_sentences SELECT * FROM sentences_parquet;
